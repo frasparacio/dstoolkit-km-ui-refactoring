533dd1be6e89d2bd7fc714470e968a0668b1c49c
Adam Black <adamblack@microsoft.com>
2023-11-23 10:42:04 +0000

    Add Tooltip descriptions to models

--- src/WebApp/src/components/chat/modelSwitch.tsx
+++ src/WebApp/src/components/chat/modelSwitch.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Text } from "@fluentui/react-components";
+import { Text, Tooltip } from "@fluentui/react-components";
 
 interface ModelSwitchProps {
     onSwitchChange: (model: string) => void;
@@ -23,32 +23,36 @@ export function ModelSwitch({ onSwitchChange, disabled }: ModelSwitchProps) {
     return (
         <div className="flex h-[70px] w-[180px] items-center justify-center rounded-lg ">
             <div className="align-center flex h-[60px] w-[150px] items-center justify-center rounded-full bg-neutral-300 shadow-md">
-                <div
-                    onClick={() => handleClick(GPT35)}
-                    className={`flex h-[50px] w-[70px] items-center justify-center rounded-full border border-neutral-300 ${
-                        activeSwitch === GPT35 ? "bg-neutral-50 shadow-lg" : "bg-neutral-300"
-                    }`}
-                >
-                    <Text
-                        className={`${activeSwitch === GPT35 ? "font-bold" : ""}`}
-                        weight={activeSwitch === GPT35 ? "bold" : "regular"}
+                <Tooltip content="Fast, efficient, and versatile" relationship="description" positioning="above-end">
+                    <div
+                        onClick={() => handleClick(GPT35)}
+                        className={`flex h-[50px] w-[70px] items-center justify-center rounded-full border border-neutral-300 ${
+                            activeSwitch === GPT35 ? "bg-neutral-50 shadow-lg" : "bg-neutral-300"
+                        }`}
                     >
-                        GPT3.5
-                    </Text>
-                </div>
-                <div
-                    onClick={() => handleClick(GPT4)}
-                    className={`flex h-[50px] w-[70px] items-center justify-center rounded-full border border-neutral-300 ${
-                        activeSwitch === GPT4 ? "bg-neutral-50 shadow-lg" : "bg-neutral-300"
-                    }`}
-                >
-                    <Text
-                        className={`${activeSwitch === GPT4 ? "font-bold" : ""}`}
-                        weight={activeSwitch === GPT4 ? "bold" : "regular"}
+                        <Text
+                            className={`${activeSwitch === GPT35 ? "font-bold" : ""}`}
+                            weight={activeSwitch === GPT35 ? "bold" : "regular"}
+                        >
+                            GPT3.5
+                        </Text>
+                    </div>
+                </Tooltip>
+                <Tooltip content="Precise, intelligent, and sophisticated" relationship="description" positioning="above-start">
+                    <div
+                        onClick={() => handleClick(GPT4)}
+                        className={`flex h-[50px] w-[70px] items-center justify-center rounded-full border border-neutral-300 ${
+                            activeSwitch === GPT4 ? "bg-neutral-50 shadow-lg" : "bg-neutral-300"
+                        }`}
                     >
-                        GPT4
-                    </Text>
-                </div>
+                        <Text
+                            className={`${activeSwitch === GPT4 ? "font-bold" : ""}`}
+                            weight={activeSwitch === GPT4 ? "bold" : "regular"}
+                        >
+                            GPT4
+                        </Text>
+                    </div>
+                </Tooltip>
             </div>
         </div>
     );
